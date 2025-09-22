853d6884bf9959ab56181388e44c1861ce79ef7f
gitXena <zina.njoku@gmail.com>
2025-09-22 20:09:40 +0100

    attempted fixing service and contact pages'

--- src/components/Home Page/ContactForm.jsx
+++ src/components/Home Page/ContactForm.jsx
@@ -48,7 +48,7 @@ export default function ContactForm() {
     e.preventDefault();
 
     try {
-      const response = await fetch(`${backendUrl}/addContact`, {
+      const response = await fetch(`${backendUrl}/contacts/addContact`, {
         method: "POST",
         headers: {
           "Content-Type": "application/json",
